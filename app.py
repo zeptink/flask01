866f5f4fb1c17f07fee47ed6cc693d5f53d4e770
zeptink <rasyidfahrizal7@gmail.com>
2025-07-19 11:10:05 +0700

    ok

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-    return "This is working From Rasyid"
+    return "This is working From Rasyid DEPOK"
  
 if __name__ == '__main__':
     app.run() 
